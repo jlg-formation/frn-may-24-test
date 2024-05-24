09daab60ccdaf5fe9a7cd37476a62a81a874fbb6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-05-24 15:13:34 +0200

    dev: test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:8081/");
+
+    cy.contains(/Choose a phot./);
+
+    cy.contains("USE this photo", {
+      matchCase: false,
+    }).click();
+
+    cy.contains("Reset").click();
+
+    cy.contains("Use this photo").click();
+
+    cy.get("[aria-label='Add']").click();
+    cy.get("[aria-label='smile']").click();
+
+    cy.get("[aria-label='image'] [aria-label='smile']").click().click();
+  });
+});
